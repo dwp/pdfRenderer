d21b4f1e07b0be0d07bd272ecb71d714882e7266
Scott Walker <scott.walker@valtech.co.uk>
2013-10-16 12:54:02 +0100

    Tests for c3's functionalTestCase9

--- test/test_data/ClaimBuilder.scala
+++ test/test_data/ClaimBuilder.scala
@@ -4127,4 +4127,234 @@ object ClaimBuilder {
       </DWPCATransaction>
     </DWPBody>
   }
+
+
+  def functionalTestCase9: Elem = {
+    <DWPBody xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns="http://www.govtalk.gov.uk/dwp/carers-allowance"
+             xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+             xsi:schemaLocation="http://www.govtalk.gov.uk/dwp/carers-allowance file:/Users/jmi/Temp/CarersAllowance_Schema.xsd">
+      <Version>0.1</Version>
+      <DWPCATransaction>
+        <TransactionId>NFM33DB</TransactionId>
+        <DateTimeGenerated>02-10-2010 14:36</DateTimeGenerated>
+        <DWPCAClaim id="TEST432">
+          <Claimant>
+            <DateOfClaim>2013-07-21</DateOfClaim>
+            <Surname>Casenine</Surname>
+            <OtherNames>Test</OtherNames>
+            <OtherSurnames></OtherSurnames>
+            <Title>mr</Title>
+            <MaritalStatus>p</MaritalStatus>
+            <DateOfBirth>1976-09-28</DateOfBirth>
+            <NationalInsuranceNumber></NationalInsuranceNumber>
+            <ExistingNationalInsuranceNumber/>
+            <Address>
+              <gds:Line>1 Preston Road</gds:Line> <gds:Line>Preston</gds:Line> <gds:Line></gds:Line> <gds:PostCode>PR1 1HB</gds:PostCode>
+            </Address>
+            <ConfirmAddress>yes</ConfirmAddress>
+            <HomePhoneNumber></HomePhoneNumber>
+            <DaytimePhoneNumber>
+              <Number></Number>
+              <Qualifier/>
+            </DaytimePhoneNumber>
+            <EmailAddress/>
+            <ClaimedBefore>Not asked</ClaimedBefore>
+          </Claimant>
+          <Caree>
+            <Surname>Caseone</Surname>
+            <OtherNames>Test</OtherNames>
+            <Title>dame</Title>
+            <DateOfBirth>1980-09-12</DateOfBirth>
+            <NationalInsuranceNumber></NationalInsuranceNumber>
+            <Address>
+              <gds:Line>1 Preston Road</gds:Line> <gds:Line>Preston</gds:Line> <gds:Line></gds:Line> <gds:PostCode>PR1 1HB</gds:PostCode>
+            </Address>
+            <ConfirmAddress>yes</ConfirmAddress>
+            <HomePhoneNumber/>
+            <DaytimePhoneNumber>
+              <Number></Number>
+              <Qualifier/>
+            </DaytimePhoneNumber>
+            <RelationToClaimant>partner</RelationToClaimant>
+            <Cared35hours>Not asked</Cared35hours>
+            <CanCareeSign>Not asked</CanCareeSign>
+            <CanSomeoneElseSign>Not asked</CanSomeoneElseSign>
+            <CanClaimantSign>Not asked</CanClaimantSign>
+            <BreaksSinceClaim>no</BreaksSinceClaim>
+
+            <Cared35hoursBefore>Not asked</Cared35hoursBefore>
+            <DateStartedCaring>2012-01-21</DateStartedCaring>
+            <BreaksBeforeClaim>no</BreaksBeforeClaim>
+            <PaidForCaring>Not asked</PaidForCaring>
+            <ClaimedPreviously>Not asked</ClaimedPreviously>
+          </Caree>
+          <ClaimADI>no</ClaimADI>
+          <Residency>
+            <Nationality>British</Nationality>
+            <EUEEASwissNational>Not asked</EUEEASwissNational>
+            <CountryNormallyLive>Not asked</CountryNormallyLive>
+            <CountryNormallyLiveOther>Not asked</CountryNormallyLiveOther>
+            <InGreatBritainNow>yes</InGreatBritainNow>
+            <InGreatBritain26Weeks>Not asked</InGreatBritain26Weeks>
+
+            <BritishOverseasPassport>Not asked</BritishOverseasPassport>
+            <OtherNationality>
+              <EUEEASwissNationalChildren/>
+              <DateArrivedInGreatBritain>Not asked</DateArrivedInGreatBritain>
+              <CountryArrivedFrom>Croatia</CountryArrivedFrom>
+              <IntendToReturn>no</IntendToReturn>
+              <DateReturn></DateReturn>
+              <VisaReferenceNumber>Not asked</VisaReferenceNumber>
+            </OtherNationality>
+            <OutOfGreatBritain>Not asked</OutOfGreatBritain>
+
+          </Residency>
+          <CourseOfEducation>no</CourseOfEducation>
+
+          <SelfEmployed>no</SelfEmployed>
+
+          <Employed>no</Employed>
+
+          <PropertyRentedOut>
+            <PayNationalInsuranceContributions/>
+            <RentOutProperty>Not asked</RentOutProperty>
+            <SubletHome>Not asked</SubletHome>
+          </PropertyRentedOut>
+          <HavePartner>no</HavePartner>
+
+          <OtherBenefits>
+            <ClaimantBenefits>
+              <JobseekersAllowance>no</JobseekersAllowance>
+              <IncomeSupport>no</IncomeSupport>
+              <PensionCredit>no</PensionCredit>
+              <StatePension>no</StatePension>
+              <IncapacityBenefit>no</IncapacityBenefit>
+              <SevereDisablementAllowance>no</SevereDisablementAllowance>
+              <MaternityAllowance>no</MaternityAllowance>
+              <UnemployabilitySupplement>no</UnemployabilitySupplement>
+              <WindowsBenefit>no</WindowsBenefit>
+              <WarWidowsPension>no</WarWidowsPension>
+              <IndustrialDeathBenefit>no</IndustrialDeathBenefit>
+              <GovernmentTrainingAllowance>no</GovernmentTrainingAllowance>
+              <LoneParentChildBenefit>no</LoneParentChildBenefit>
+              <OtherSocialSecurityBenefit>Not asked</OtherSocialSecurityBenefit>
+              <NonSocialSecurityBenefit>Not asked</NonSocialSecurityBenefit>
+              <NoBenefits>Not asked</NoBenefits>
+            </ClaimantBenefits>
+            <PartnerBenefits>
+              <JobseekersAllowance>no</JobseekersAllowance>
+              <IncomeSupport>no</IncomeSupport>
+              <PensionCredit>no</PensionCredit>
+              <StatePension>no</StatePension>
+              <IncapacityBenefit>no</IncapacityBenefit>
+              <SevereDisablementAllowance>no</SevereDisablementAllowance>
+              <MaternityAllowance>no</MaternityAllowance>
+              <UnemployabilitySupplement>no</UnemployabilitySupplement>
+              <WindowsBenefit>no</WindowsBenefit>
+              <WarWidowsPension>no</WarWidowsPension>
+              <IndustrialDeathBenefit>no</IndustrialDeathBenefit>
+              <GovernmentTrainingAllowance>no</GovernmentTrainingAllowance>
+              <OtherSocialSecurityBenefit>Not asked</OtherSocialSecurityBenefit>
+              <NonSocialSecurityBenefit>Not asked</NonSocialSecurityBenefit>
+              <NoBenefits>Not asked</NoBenefits>
+            </PartnerBenefits>
+            <ExtraMoney>Not asked</ExtraMoney>
+            <OtherMoneySSP>no</OtherMoneySSP>
+            <OtherMoneySMP>no</OtherMoneySMP>
+          </OtherBenefits>
+          <Payment>
+            <PaymentFrequency>Not asked</PaymentFrequency>
+            <InitialAccountQuestion>Not asked</InitialAccountQuestion>
+
+          </Payment>
+          <OtherInformation/>
+          <ThirdParty>no</ThirdParty>
+          <Declaration>
+
+
+            <TextLine>Do you agree to us getting information from any other person or organisation you have told us about as part of this claim? Yes</TextLine>
+
+
+            <TextLine>This is my claim for Carer's Allowance.</TextLine>
+            <TextLine>I understand that if I am paid Carer's Allowance it may affect the benefits paid to Test Casenine.</TextLine>
+            <TextLine>If the person you are caring for receives certain benefits, the amount they receive may be affected by your claim for Carer's Allowance. Because of this we need both of you to understand the potential consequences of your claim to Carer's Allowance.</TextLine>
+            <TextLine>If Test Casenine gets a Severe Disability Premium with their income-based Jobseeker's Allowance, Income Support, income-related Employment and Support Allowance, Housing Benefit, they may no longer get that premium if we pay Carer's Allowance to you.</TextLine>
+            <TextLine>If Test Casenine's Pension Credit includes an extra amount for severe disability, they may no longer get that extra amount if we pay Carer's Allowance to you.</TextLine>
+            <TextLine>This could also affect any reduction in Council Tax Test Casenine may be entitled to. To find out more about it, please contact the Local Authority.</TextLine>
+            <TextLine>We will need to check Test Casenine entitlement to Disability Living Allowance, Personal Independence Payment, Attendance Allowance, Constant Attendance Allowance or Armed Forces Independence Payment when considering your claim.</TextLine>
+            <TextLine>We may contact Test Casenine or their representative to establish whether 35 hours caring per week is taking place.</TextLine>
+            <TextLine></TextLine>
+            <TextLine>Please tick this box to declare that you have understood the notes and you have made / will make the person you are caring for / or their representative aware that there could be a change to their benefits. = yes</TextLine>
+
+            <TextLine>I declare that I understand the Carer's Allowance Claim Notes and that the information provided on this claim form is correct and complete.</TextLine>
+            <TextLine>I understand that I must report all changes in my circumstances or that of the person that I am caring for which may affect my entitlement promptly and by failing to do so I may be liable to prosecution or face a financial penalty.</TextLine>
+            <TextLine>I will phone 08456084321 or write to Carer's Allowance Unit, Palatine House, Preston, Lancaster, PR1 1HB to report a change in my circumstances or that of the person that I am caring for.</TextLine>
+            <TextLine>If I give false or incomplete information or fail to report changes in my circumstances or that of the person that I am caring for promptly, I understand that my Carer's Allowance may be stopped or reduced and any overpayment of Carer's Allowance may be recovered. In addition I may be prosecuted or face a financial penalty.</TextLine>
+            <TextLine>We may wish to contact any current or previous employers, or other persons or organisations you have listed on this claim form to obtain information about your claim. You do not have to agree to this but if you do not, it may mean that we are unable to obtain enough information to satisfy ourselves that you meet the conditions of entitlement for your claim.</TextLine>
+            <TextLine></TextLine>
+            <TextLine>Please tick this box to confirm that you understand and make the declarations above. = yes</TextLine>
+            <TextLine>Please tick this box if this claim form has been filled in by someone else, if so, please ensure that you understand the declarations above as another person cannot make the declarations on your behalf. = yes</TextLine>
+
+            <TextLine>Do you live in Wales and would like to receive future communications in Welsh? No</TextLine>
+          </Declaration>
+          <EvidenceList>
+            <TextLine>
+              XML Generated at: 2013-10-16T12:51:49
+            </TextLine> <TextLine>
+            ======================About You======================
+          </TextLine> <TextLine>
+            Have you always lived in the UK? = No
+          </TextLine> <TextLine/> <TextLine>
+            Are you currently living in the UK? = Yes
+          </TextLine> <TextLine>
+            When did you arrive in the UK? = 21/07/2012
+          </TextLine> <TextLine>
+            Do you get state Pension? = Yes
+          </TextLine> <TextLine/> <TextLine>
+            ================About Care You Provide================
+          </TextLine> <TextLine>
+            Do they live at the same address as you? = Yes
+          </TextLine> <TextLine>
+            Does this person get Armed Forces Independence Payment? = Yes
+          </TextLine> <TextLine>
+            Do you spend 35 hours or more each week caring for this person? = Yes
+          </TextLine> <TextLine>
+            Did you care for this person for 35 hours or more each week before your claim date ? = Yes
+          </TextLine> <TextLine>
+            =====================Time abroad=====================
+          </TextLine> <TextLine>
+            Do you normally live in the UK, Republic of Ireland, Isle of Man or the Channel Islands? = Yes
+          </TextLine> <TextLine>
+            Have you had any more trips out of Great Britain for more than 52 weeks at a time, since 21/07/2013 (this is 156 weeks before your claim date)? = No
+          </TextLine> <TextLine>
+            Have you been out of Great Britain with the person you care for, for more than four weeks at a time, since 21/07/2013 (this is 3 years before your claim date)? = No
+          </TextLine> <TextLine>
+            =====================Other Money=====================
+          </TextLine> <TextLine>
+            Have you
+            &lt;
+            or your partner/spouse
+            &gt;
+            claimed or received any other benefits since the date you want to claim? = No
+          </TextLine> <TextLine>
+            Have you received any payments for the person you care for or any other person since your claim date? = No
+          </TextLine> <TextLine/>
+            <TextLine/>
+            <TextLine/>
+            <TextLine/>
+            <TextLine/>
+            <TextLine/>
+            <TextLine/>
+            <TextLine/>
+            <TextLine/>
+            <TextLine/> <TextLine>
+            Are you, your wife, husband, civil partner or parent you are dependent on, receiving any pensions or benefits from another EEA State or Switzerland? = No
+          </TextLine> <TextLine>
+            Are you, your wife, husband, civil partner or parent you are dependent on working in or paying insurance to another EEA State or Switzerland? = No
+          </TextLine>
+          </EvidenceList>
+        </DWPCAClaim>
+      </DWPCATransaction>
+    </DWPBody>
+  }
 }
