a99606cd91f6ecabcb1bc31dfe9fa0d3f329d3f8
Scott Walker <scott.walker@valtech.co.uk>
2013-10-16 11:58:14 +0100

    Tests for c3's functionalTestCase1

--- test/generators/PdfGeneratorSpec.scala
+++ test/generators/PdfGeneratorSpec.scala
@@ -52,6 +52,22 @@ class PdfGeneratorSpec extends Specification with Before{
     pdfFile.exists() must beTrue
   }
 
+  "PdfGenerator should be handle functionalTestCase1 and return success"  should {
+    val xml = ClaimBuilder.functionalTestCase1
+    val dataSource = new XmlDataSource(xml)
+    val generator = PdfGenerator(dataSource)
+    val generatorResult = generator.generateFrom()
+    generatorResult must beAnInstanceOf[GenerationSuccess]
+  }
+
+  "PdfGenerator should create functionalTestCase1 PDF file" should {
+    val xml = ClaimBuilder.functionalTestCase1
+    val dataSource = new XmlDataSource(xml)
+    PdfGenerator(dataSource).generateFrom()
+    val pdfFile = new File(PdfGenerator.pdfFileLocation)
+    pdfFile.exists() must beTrue
+  }
+
        /*
   "Extract PDF" should {
     val reader = new com.itextpdf.text.pdf.PdfReader("/Users/valtechuk/test.pdf")
