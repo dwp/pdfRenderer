b7c27315dc91423f9966d2809731ab82bbdf780d
PrafullaC <prafulla.chandra@valtech.co.uk>
2013-10-18 12:18:06 +0100

    Broken down the mandatory fields

--- test/test_data/XMLData.scala
+++ test/test_data/XMLData.scala
@@ -59,23 +59,40 @@ object XMLData {
     val fields = XMLDataFields(xml)
     Seq(
       "Transaction: " + fields.transactionPath.text + " " + fields.title.text + " " + fields.surName.text + " " + fields.nationalInsuranceNumber.text,
-      "Summary",
-      "About you - the carer",
+      "Summary"
+    ) ++ aboutYouTheCarer(fields) ++ aboutTheCareYouProvide(fields) ++ claimDates(fields) ++ claimSummary(fields)
+  }
+
+  def aboutYouTheCarer(fields: XMLDataFields) = {
+    Seq("About you - the carer",
       "National Insurance Number " + fields.nationalInsuranceNumber.text,
       "Last name " + fields.surName.text,
       "First name(s) " + fields.firstName.text,
       "Title " + fields.title.text,
       "Street / Town / City " + fields.addressCarer,
-      "Postcode " + fields.postCodeCarer.text,
+      "Postcode " + fields.postCodeCarer.text
+    )
+  }
+
+  def aboutTheCareYouProvide(fields: XMLDataFields) = {
+    Seq(
       "About the care you provide",
       "Last name " + fields.careeLastName.text,
       "First name(s) " + fields.careeFirstName.text,
       "Title " + fields.careeTitle.text,
       "Street / Town / City " + fields.addressCaree,
-      "Postcode " + fields.postCodeCaree.text,
-      "Claim Dates",
-      "Date claim received " + fields.dateClaimReceived.text,
-      "Claim Summary",
+      "Postcode " + fields.postCodeCaree.text
+    )
+  }
+
+  def claimDates(fields: XMLDataFields) = {
+    Seq("Claim Dates",
+      "Date claim received " + fields.dateClaimReceived.text
+    )
+  }
+
+  def claimSummary(fields: XMLDataFields) = {
+    Seq("Claim Summary",
       "Transaction: " + fields.transactionPath.text + " " + "Claim received: " + fields.dateClaimReceived.text
     )
   }
