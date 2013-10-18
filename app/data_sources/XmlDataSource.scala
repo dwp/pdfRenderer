7f33b729eb153c4dff608d35c85dfa2a932a1ddf
Scott Walker <scott.walker@valtech.co.uk>
2013-10-18 14:15:08 +0100

    Refactor to move strings and xpaths  into vals

--- app/data_sources/XmlDataSource.scala
+++ app/data_sources/XmlDataSource.scala
@@ -6,20 +6,25 @@ import java.io.ByteArrayInputStream
 import net.sf.jasperreports.engine.data.JRXmlDataSource
 
 case class XmlDataSource(val source:Elem) extends ReportDataSource {
+  val claimElement = source \\ "DWPCATransaction" \\ "DWPCAClaim"
+  val claimJasperTemplate = "reportNewClaim"
+  val circsElement = source \\ "DWPCATransaction" \\ "DWPCAChangeOfCircumstances"
+  val circsJasperTemplate = "reportNewCircs"
+
 
   def convertToJRDataSource(): JRDataSource = {
     new JRXmlDataSource(new ByteArrayInputStream(source.toString().getBytes("UTF-8")))
   }
 
   def jasperReportFilenameMatcher(): String = {
-    (source \\ "DWPCATransaction" \\ "DWPCAChangeOfCircumstances").length match {
-      case 0 => {
-        (source \\ "DWPCATransaction" \\ "DWPCAClaim").length match {
-          case 0 => throw new scala.RuntimeException("XmlDataSource could not match a report template")
-          case _ => "reportNewClaim"
+    circsElement.isEmpty match {
+      case true => {
+        claimElement.isEmpty match {
+          case true => throw new scala.RuntimeException("XmlDataSource did not contain an XML element that matches a jasper template")
+          case false => claimJasperTemplate
         }
       }
-      case _ => "reportNewCircs"
+      case false => circsJasperTemplate
     }
   }
 }
