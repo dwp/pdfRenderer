778055235617892c57d95789bf7a5b37f60135b8
Scott Walker <scott.walker@valtech.co.uk>
2013-10-18 14:07:51 +0100

    Tests for jasperReportFilenameMatcher return correct value depending on xml element

--- test/data_sources/XmlDataSourceSpec.scala
+++ test/data_sources/XmlDataSourceSpec.scala
@@ -9,10 +9,29 @@ import net.sf.jasperreports.engine.data.JRXmlDataSource
  *         Date: 11/10/2013
  */
 class XmlDataSourceSpec  extends Specification {
+  "XmlDataSourceSpec" should {
+    "An XmlDataSource should convert XML to JRXmlDataSource" in {
+      val xml = <DWPCAClaim><DWPCATransaction>ER123DF</DWPCATransaction></DWPCAClaim>
+      val dataSource = new XmlDataSource(xml)
+      dataSource.convertToJRDataSource() must beAnInstanceOf[JRXmlDataSource]
+    }
 
-  "An XmlDataSource should convert XML to JRXmlDataSource"  should {
-    val xml = <DWPCAClaim><DWPCATransaction>ER123DF</DWPCATransaction></DWPCAClaim>
-    val dataSource = new XmlDataSource(xml)
-    dataSource.convertToJRDataSource() must beAnInstanceOf[JRXmlDataSource]
+    "jasperReportFilenameMatcher returns reportNewCircs for xml containing DWPCAChangeOfCircumstances element" in {
+      val xml = <DWPCAClaim><DWPCATransaction><DWPCAChangeOfCircumstances></DWPCAChangeOfCircumstances></DWPCATransaction></DWPCAClaim>
+      val dataSource = new XmlDataSource(xml)
+      dataSource.jasperReportFilenameMatcher() mustEqual "reportNewCircs"
+    }
+
+    "jasperReportFilenameMatcher returns reportNewClaim for xml containing DWPCAClaim element" in {
+      val xml = <DWPCAClaim><DWPCATransaction><DWPCAClaim></DWPCAClaim></DWPCATransaction></DWPCAClaim>
+      val dataSource = new XmlDataSource(xml)
+      dataSource.jasperReportFilenameMatcher() mustEqual "reportNewClaim"
+    }
+
+    "jasperReportFilenameMatcher throws for xml not containing DWPCAChangeOfCircumstances or DWPCAClaim element" in {
+      val xml = <DWPCAClaim><DWPCATransaction></DWPCATransaction></DWPCAClaim>
+      val dataSource = new XmlDataSource(xml)
+      dataSource.jasperReportFilenameMatcher() must throwA[scala.RuntimeException]
+    }
   }
 }
