5672cad34d4bb91e3adc4749a9b005fc61b2ed51
Scott Walker <scott.walker@valtech.co.uk>
2013-10-22 11:17:43 +0100

    Removed default comment header

--- test/generators/Helper.scala
+++ test/generators/Helper.scala
@@ -4,13 +4,6 @@ import java.io.File
 import scala.xml.Elem
 import data_sources.XmlDataSource
 
-/**
- * Created with IntelliJ IDEA.
- * User: valtechuk
- * Date: 21/10/2013
- * Time: 11:22
- * To change this template use File | Settings | File Templates.
- */
 object Helper {
   def deletePdfFile(pdfFileLocation: String): Unit = {
     val pdfFile = new File(pdfFileLocation)
