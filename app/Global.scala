a8cf460343e03407812c2294cac35fec4609c193
Jorge Migueis <jorge.migueis@valtech.co.uk>
2013-11-14 11:53:04 +0000

    Removed unnecessary entries from configuration file. Change logging at startup and shutdown to identify correctly the PDF service.

--- app/Global.scala
+++ app/Global.scala
@@ -8,11 +8,11 @@ object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
     super.onStart(app)
-    Logger.info("s2 Started") // used for operations, do not remove
+    Logger.info("PDFService Started") // used for operations, do not remove
   }
 
   override def onStop(app: Application) {
     super.onStop(app)
-    Logger.info("s2 Stopped") // used for operations, do not remove
+    Logger.info("PDFService Stopped") // used for operations, do not remove
   }
 }
