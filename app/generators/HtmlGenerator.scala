e963c1755201c96bf51402a829355fd9d924118d
Ruben Diaz <ruben.diaz@valtech.co.uk>
2014-05-01 14:18:47 +0100

    US679 - Changing the way we show the pictures of the summary page.

--- app/generators/HtmlGenerator.scala
+++ app/generators/HtmlGenerator.scala
@@ -16,9 +16,6 @@ object HtmlGenerator extends ReportGenerator {
     try {
       if (print.isDefined) {
         val exporter = new HtmlExporter()
-        exporter.setParameter(JRHtmlExporterParameter.IMAGES_DIR, new File("/tmp"));
-        exporter.setParameter(JRHtmlExporterParameter.IMAGES_URI, "/tmp");
-        exporter.setParameter(JRHtmlExporterParameter.IS_OUTPUT_IMAGES_TO_DIR, true);
         exporter.setParameter(JRExporterParameter.JASPER_PRINT, print.get)
         exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, stream)
 //        exporter.setParameter(JRHtmlExporterParameter.IS_USING_IMAGES_TO_ALIGN, false);
