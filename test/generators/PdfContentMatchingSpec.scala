7ccdf676b7a514773651b6a55129494aa1d64b73
Mark Hunter <mark@huntersix.co.uk>
2014-06-05 11:15:43 +0100

    US524 - Increase functional tests to match business tests on CASA, fix report fields to point to correct part of XML document

--- test/generators/PdfContentMatchingSpec.scala
+++ test/generators/PdfContentMatchingSpec.scala
@@ -57,6 +57,6 @@ trait PdfContentMatchingSpec extends Specification{
     val totalContent = getPDFContent(pdfFileLocation)
     val testData = generateTestData(testCaseXml)
     matchFunction(testData, totalContent) must beTrue
-    deleteFile(pdfFileLocation)
+//    deleteFile(pdfFileLocation)
   }
 }
