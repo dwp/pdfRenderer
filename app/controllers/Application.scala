af7a7184682b75237d873968a67452959f39ed4b
Jorge Migueis <jorge.migueis@valtech.co.uk>
2014-08-05 11:05:43 +0100

    Removed unused configuration entries.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,20 +1,21 @@
 package controllers
 
-import play.api.mvc._
-import service.RenderService
-import scala.util.{Success, Try}
-import play.api.{Logger, Play}
-import generators.{HtmlGenerator, PdfGenerator, ReportGenerator}
 import java.io.{ByteArrayOutputStream, FileOutputStream, OutputStream}
 import java.text.SimpleDateFormat
 import java.util.Date
+
+import app.ConfigProperties._
+import generators.{HtmlGenerator, PdfGenerator, ReportGenerator}
+import play.api.Logger
+import play.api.mvc._
+import service.RenderService
 
 
 object Application extends Controller {
 
   def generatePDF = Action { request =>
     val service = new RenderService {
-      lazy val pdfLocation = Try(Play.current.configuration.getString("pdf.folder").getOrElse("./")) match {case Success(s) => s; case _ => "./"}
+      lazy val pdfLocation = getProperty("pdf.folder","./")
       protected def reportGenerator: ReportGenerator = PdfGenerator
       protected def content: String = ""
       protected val outputStream: OutputStream = new FileOutputStream(s"${pdfLocation}PDFGenerated_${new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date())}.pdf")
