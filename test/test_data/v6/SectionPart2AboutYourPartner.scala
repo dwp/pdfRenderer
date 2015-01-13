ac73a22228e15fa79347f6ffbd791697c3000341
PrafullaC <prafulla.chandra@valtech.co.uk>
2015-01-13 15:42:36 +0000

    US993 - Remove <OtherSurnames> from about you the carer

---
+++ test/test_data/v6/SectionPart2AboutYourPartner.scala
@@ -0,0 +1,23 @@
+package test_data.v6
+
+import scala.xml.Elem
+
+
+case class SectionPart2AboutYourPartner(xml: Elem) {
+
+  val rootPath = xml \\ "DWPCATransaction" \\ "DWPCAClaim" \\ "Partner"
+
+  val otherSurnameOrMaidenName = rootPath \\ "OtherSurnames"
+
+  val dateOfBirth = rootPath \\ "DateOfBirth"
+
+  val nationalityPartner = rootPath \\ "NationalityPartner"
+
+  val seperatedFromPartnerQuestion = rootPath \\ "RelationshipStatus" \\ "SeparatedFromPartner" \\ "QuestionLabel"
+
+  val seperatedFromPartnerAnswer = rootPath \\ "RelationshipStatus" \\ "SeparatedFromPartner" \\ "Answer"
+
+  val isCareeQuestion = rootPath \\ "IsCaree" \\ "QuestionLabel"
+
+  val isCareeAnswer = rootPath \\ "IsCaree" \\ "Answer"
+}
