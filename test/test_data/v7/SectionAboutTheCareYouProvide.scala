26474ca79c74a738b0e8812b988f15b6920444c3
PrafullaC <prafulla.chandra@valtech.co.uk>
2015-01-19 12:45:53 +0000

    US1059 - New version 0.7 due to schema change

---
+++ test/test_data/v7/SectionAboutTheCareYouProvide.scala
@@ -0,0 +1,54 @@
+package test_data.v7
+
+import scala.xml.Elem
+
+
+case class SectionAboutTheCareYouProvide(xml: Elem) {
+
+  val rootPath = xml \\ "DWPCATransaction" \\ "DWPCAClaim" \\ "Caree"
+
+  val nationalInsuranceNumber = rootPath \\ "NationalInsuranceNumber"
+
+  val dateOfBirth = rootPath \\ "DateOfBirth"
+
+  val liveSameAddressQuestion = rootPath \\ "LiveSameAddress" \\ "QuestionLabel"
+
+  val liveSameAddressAnswer = rootPath \\ "LiveSameAddress" \\ "Answer"
+
+  val dayTimeTelephoneNumber = rootPath \\ "DayTimePhoneNumber"
+
+  val relationToClaimantQuestion = rootPath \\ "RelationToClaimant" \\ "QuestionLabel"
+
+  val relationToClaimantAnswer = rootPath \\ "RelationToClaimant" \\ "Answer"
+
+  val armedForcesIndependantPaymentQuestion = rootPath \\ "ArmedForcesIndependencePayment" \\ "QuestionLabel"
+
+  val armedForcesIndependantPaymentAnswer = rootPath \\ "ArmedForcesIndependencePayment" \\ "Answer"
+
+  val cared35HoursQuestion = rootPath \\ "Cared35Hours" \\ "QuestionLabel"
+
+  val cared35HoursAnswer = rootPath \\ "Cared35Hours" \\ "Answer"
+
+  val cared35HoursBeforeQuestion = rootPath \\ "Cared35HoursBefore" \\ "QuestionLabel"
+
+  val cared35HoursBeforeAnswer = rootPath \\ "Cared35HoursBefore" \\ "Answer"
+
+  val dateStartedCaringQuestion = rootPath \\ "DateStartCaring" \\ "QuestionLabel"
+
+  val dateStartedCaringAnswer = rootPath \\ "DateStartCaring" \\ "Answer"
+
+  val breaksInCareQuestion = rootPath \\ "BreaksSinceClaim" \\ "QuestionLabel"
+
+  val breaksInCareAnswer = rootPath \\ "BreaksSinceClaim" \\ "Answer"
+
+  val careeLastName = rootPath  \\ "Surname"
+
+  val careeFirstName = rootPath  \\ "OtherNames"
+
+  val careeTitle = rootPath  \\ "Title"
+
+  val addressCaree = (rootPath \\ "Address" \\ "Line").map(x => x.text).filterNot(x => x.isEmpty).mkString(" ")
+
+  val postCodeCaree = rootPath \\ "Address" \\ "PostCode"
+
+}
