42fbdc272031549e3336c232ec555c5b07c727dd
PrafullaC <prafulla.chandra@valtech.co.uk>
2015-02-20 15:59:29 +0000

    US1119 - Removed 'ArmedForcesIndependencePayment' question from care you provide

---
+++ test/test_data/v9/SectionAboutYourPayDetails.scala
@@ -0,0 +1,26 @@
+package test_data.v9
+
+import scala.xml.Elem
+
+case class SectionAboutYourPayDetails(xml: Elem) {
+
+  val rootPath = xml \ "DWPCATransaction" \ "DWPCAClaim"
+
+  val howToGetPaidQuestion = rootPath \ "Payment" \ "InitialAccountQuestion" \ "QuestionLabel"
+  val howToGetPaidAnswer = rootPath \ "Payment" \ "InitialAccountQuestion" \ "Answer"
+
+  val howOftenGetPaidQuestion = rootPath \ "Payment" \ "PaymentFrequency" \ "QuestionLabel"
+  val howOftenGetPaidAnswer = rootPath \ "Payment" \ "PaymentFrequency" \ "Answer"
+  val howOftenGetPaidOther = rootPath \ "Payment" \ "PaymentFrequency" \ "Other"
+
+  val bankAccountHolderName = rootPath \ "Payment" \ "Account" \ "HolderName"
+
+  val bankAccountBankName = rootPath \ "Payment" \ "Account" \ "BuildingSocietyDetails" \ "Name"
+
+  val bankAccountSortCode = rootPath \ "Payment" \ "Account" \ "BuildingSocietyDetails" \ "Sortcode"
+
+  val bankAccountNumber = rootPath \ "Payment" \ "Account" \ "BuildingSocietyDetails" \ "AccountNumber"
+
+  val bankAccountReferenceNumber = rootPath \ "Payment" \ "Account" \ "BuildingSocietyDetails" \ "RollNumber"
+
+}
