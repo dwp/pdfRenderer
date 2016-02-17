ea084b93a7ccf05bd1117786f3546d783bdf0024
David Hutchinson <david@scrumconnect.com>
2016-02-17 13:15:46 +0000

    bumped version to 2.5

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import play.sbt.Play.autoImport._
 object ApplicationBuild extends Build {
 
   val appName = "p1"
-  val appVersion = "2.4-SNAPSHOT"
+  val appVersion = "2.5-SNAPSHOT"
 
   var repo: Seq[Def.Setting[_]] = Seq(resolvers += "Carers repo" at "http://build.3cbeta.co.uk:8080/artifactory/repo/",
   resolvers += "Jaspersoft repo" at "http://jaspersoft.artifactoryonline.com/jaspersoft/third-party-ce-artifacts/",
