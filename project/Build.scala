c77094e604131fb4912b98250fc88748a53cac1f
David Hutchinson <david@scrumconnect.com>
2016-04-12 10:25:47 +0100

    updated conf/application-info.conf

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import utils.ConfigurationChangeHelper._
 
 object ApplicationBuild extends Build {
   val appName = "p1"
-  val appVersion = "2.8-SNAPSHOT"
+  val appVersion = "2.9-SNAPSHOT"
 
   processConfFiles(Seq("conf/application-info.conf"), Seq("application.version" -> appVersion, "application.name" -> appName))
 
